60de40cfddac980846cd592bd4d6eb5319900fee
unknown <adibcse10@gmail.com>
2025-07-11 03:00:04 +0600

    added category crud in backend

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+use function Pest\Laravel\delete;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::withCount('courses')->latest()->paginate(10);
+        return Inertia::render('admin/categories', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255|unique:categories',
+        ]);
+
+        Category::create($validated);
+
+        return redirect()->route('admin.categories.index')
+        ->with('success', 'Category Added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+        $category = Category::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name'.$id,
+        ]);
+
+        $category->update($validated);
+        return redirect()->route('admin.categories.index')
+        ->with('success', 'Category Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::findOrFail($id);
+
+        if ($category->courses()->count() > 0) {
+            return redirect()->route('admin.categories.index')
+        ->with('success', 'Category Cannot be Deleted Because Courses Are associated with it!');
+        }
+
+        $category->destroy();
+
+        return redirect()->route('admin.categories.index')
+        ->with('success', 'Category Deleted Successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -12,6 +13,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('categories', CategoryController::class)->except(['show']);
+});
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
