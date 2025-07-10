d29d7f32451a6cee42b38fc32637a77a275bf207
unknown <adibcse10@gmail.com>
2025-07-11 01:05:20 +0600

    added all important model relations like one to many, has through many, belongs to etc accross models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-   //
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function modules()
+    {
+        return $this->hasMany(Module::class);
+    }
+
+    public function lessons()
+    {
+        return $this->hasManyThrough(Lesson::class, Module::class,'course_id', 'module_id', 'id', 'id');
+    }
 }
--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+
+    public function lessons() {
+        return $this->hasMany(Lesson::class);
+    }
+
+    public function course() {
+        return $this->belongsToy(Course::class);
+    }
 }
